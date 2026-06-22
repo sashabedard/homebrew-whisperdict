8b9b365546a06df27466e071281c38d7bbbcc76e
Sasha Bédard <85566308+sashabedard@users.noreply.github.com>
2026-06-22 11:33:05 -0400

    pith 1.0.3

--- Casks/pith.rb
+++ Casks/pith.rb
@@ -1,6 +1,6 @@
 cask "pith" do
-  version "1.0.2"
-  sha256 "194259349f14a6137ff2ddca74c2d3e9d0b04612bb89bfd4c71bbe245ca9a18d"
+  version "1.0.3"
+  sha256 "189adb3db154da9179bab59acd240f980e4c54fca7476ff6498f5314b131fc14"
 
   url "https://github.com/sashabedard/Pith/releases/download/v#{version}/Pith-#{version}.dmg"
   name "Pith"
